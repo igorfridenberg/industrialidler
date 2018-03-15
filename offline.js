9f18cd64670e78790f1962d51b89c0de700d91f3
Igor <igor.fridenberg@gmail.com>
2018-03-16 00:13:37 +0300

    initcom

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1521148254,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite_ironore-sheet0.png",
+		"images/sprite_forest-sheet0.png",
+		"images/sprite_meltingirontimer-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
